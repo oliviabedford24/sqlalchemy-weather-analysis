2f8610d713718a918e0a809e1b1735dfae9c648d
Olivia Bedford <olivia.traveler.524@gmail.com>
2023-03-02 21:21:40 -0600

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # sqlalchemy-challenge
+
+This is the Module 10: SQL Alchemy challenge for the Rice University Data Visualization bootcamp. In this challenge, we used SQL Alchemy to analyze and visualize data from weather stations in Hawai'i. 
+
+In the "Resources" folder is contained all of the given csv and sqlite files containing the data I analyzed for this challenge. You can just ignore those files, unless you just really enjoy looking at raw data for some reason (but you do you, boo).
+
+In the "SurfsUp" file are all of the code I personally wrote for this challenge. The "climate_starter" file contains the SQL Alchemy code where I sorted and analyzed the data, as well as two graphs displaying the data. The "app" file contains the Flask code I wrote to create a web app for queries about the weather in Hawai'i.
+
+The code contained in this repository is mine, and mine alone. I did not collaborate with anyone for this challenge, but did receive some help from my instructors and Rice University's team of tutors.
+
+Another day, another slay. :D
---
+++ SurfsUp/app.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+
+measurement = Base.classes.measurement
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    """List all available API routes"""
+    return (f"Welcome to the Hawaii Weather API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br>"
+        f"/api/v1.0/start/end<br>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    """Return hawaii precipitation dictionary"""
+    results = session.query(measurement.date, measurement.prcp).order_by(measurement.date).all()
+    session.close()
+    all_prcp_data = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        all_prcp_data.append(prcp_dict)
+    return jsonify(all_prcp_data)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    """Return station data as list"""
+    results = session.query(measurement.station).all()
+    session.close()
+    all_stations = list(np.ravel(results))
+    return jsonify(all_stations)
+
+tobs_date = dt.date(year=2016, month=1, day=1)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    results = session.query(measurement.date, measurement.tobs).\
+        filter(measurement.station == 'USC00519281').\
+        filter(measurement.date >= tobs_date).all()
+    session.close()
+    temp_obs = list(np.ravel(results))
+    return jsonify(temp_obs)
+
+start_date = dt.date(year=2014, month=5, day=24)
+
+@app.route("/api/v1.0/start")
+def start():
+    session = Session(engine)
+    results = session.query(func.min(measurement.tobs), func.max(measurement.tobs),
+        func.avg(measurement.tobs)).\
+        filter(measurement.date >= start_date).all()
+    session.close()
+    start_date = list(np.ravel(results))
+    return jsonify(start_date)
+
+second_start = dt.date(year=2014, month=1, day=1)
+end_date = dt.date(year=2016, month=12, day=30)
+
+@app.route("/api/v1.0/start/end")
+def start_end():
+    session = Session(engine)
+    results = session.query(func.min(measurement.tobs), func.max(measurement.tobs),
+        func.avg(measurement.tobs)).\
+        filter(measurement.date >= second_start, measurement.date <= end_date).all()
+    session.close()
+    start_end_date = list(np.ravel(results))
+    return jsonify(start_end_date)
+
+if __name__ == "__main__":
+    app.run()
